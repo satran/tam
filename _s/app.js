57e85c0142312e61e39a2b237f87ebf4f86b070c
Satyajit Ranjeev <s@ranjeev.in>
2022-02-26 10:32:36 +0100

    finalise layout

--- _s/app.js
+++ _s/app.js
@@ -2,14 +2,28 @@ var a;
 (function() {
 
     var CardDetailView = Backbone.View.extend({
-        template: _.template($("#card-view-template").html()),
+        template: _.template($("#card-detail-view-template").html()),
+        className: "card-detail-col",
+
+        events : {
+            "click .back-btn": "back" 
+        },
+
+        back: function() {
+            window.history.back();
+        },
 
         initialize: function(options) {
-            this.el = options.el;
+            this.model = {
+                id: 1,
+                title: "Hello world",
+                description: "Web applications often provide linkable, bookmarkable, shareable URLs for important locations in the app. Until recently, hash fragments (#page) were used to provide these permalinks, but with the arrival of the History API, it's now possible to use standard URLs (/page). Backbone.Router provides methods for routing client-side pages, and connecting them to actions and events. For browsers which don't yet support the History API, the Router handles graceful fallback and transparent translation to the fragment version of the URL. Getting Started When working on a web application that involves a lot of JavaScript, one of the first things you learn is to stop tying your data to the DOM. It's all too easy to create JavaScript applications that end up as tangled piles of jQuery selectors and callbacks, all trying frantically to keep data in sync between the HTML UI, your JavaScript logic, and the database on your server. For rich client-side applications, a more structured approach is often helpful. With Backbone, you represent your data as Models, which can be created, validated, destroyed, and saved to the server. Whenever a UI action causes an attribute of a model to change, the model triggers a change event; all the Views that display the model's state can be notified of the change, so that they are able to respond accordingly, re-rendering themselves with the new information. In a finished Backbone app, you don't have to write the glue code that looks into the DOM to find an element with a specific id, and update the HTML manually — when the model changes, the views simply update themselves. Philosophically, Backbone is an attempt to discover the minimal set of data-structuring (models and collections) and user interface (views and URLs) primitives that are generally useful when building web applications with JavaScript. In an ecosystem where overarching, decides-everything-for-you frameworks are commonplace, and many libraries require your site to be reorganized to suit their look, feel, and default behavior — Backbone should continue to be a tool that gives you the freedom to design the full experience of your web application. ",
+            };
         },
 
         render: function() {
-            this.el.html(this.template());
+            this.$el.html(this.template(this.model));
+            return this;
         }
     });
 
@@ -26,12 +40,14 @@ var a;
 
     var SearchView = Backbone.View.extend({
         template: _.template($("#search-view-template").html()),
+        className: "search-col",
+        tagName: "div",
 
         initialize: function(options) {
             this.cards = [
-                { title: "hello world" },
-                { title: "Another card" },
-                { title: "Yet Another card" }
+                { id: 1, title: "hello world" },
+                { id: 1, title: "Another card" },
+                { id: 1, title: "Yet Another card" }
             ];
         },
 
@@ -42,6 +58,7 @@ var a;
                 var v = new CardSummaryView({ model: card });
                 parent.$el.find('.items').append(v.render().$el);
             });
+            return this;
         }
     });
 
@@ -82,6 +99,7 @@ var a;
         el: $("#app"),
 
         routes: {
+            "": "favourites",
             "fav": "favourites",
             "s/:query": "search",
             "c/:id": "card",
@@ -93,13 +111,13 @@ var a;
         },
 
         search: function(query) {
-            let view = new SearchView({ el: this.el });
-            view.render();
+            let view = new SearchView();
+            this.el.html(view.render().el);
         },
 
         card: function(id) {
-            let view = new CardDetailView({ el: this.el });
-            view.render();
+            let view = new CardDetailView();
+            this.el.html(view.render().el);
         }
     });
 
